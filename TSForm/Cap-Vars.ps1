9a50f4e71c8c2e511453bcf6eefe4582f52538c4
kirky007 <39325019+kirky007@users.noreply.github.com>
2025-02-10 01:33:17 +1100

    Update Cap-Vars.ps1

--- TSForm/Cap-Vars.ps1
+++ TSForm/Cap-Vars.ps1
@@ -69,33 +69,46 @@
     # Define the form
     $form = New-Object System.Windows.Forms.Form
     $form.Text = "Deployed ICT Server Configuration Form"
-    $form.Size = New-Object System.Drawing.Size(550,520)
+    $form.Size = New-Object System.Drawing.Size(490,520)
     $form.StartPosition = "CenterScreen"
+    # Set FormBorderStyle to FixedToolWindow which removes minimize and maximize buttons
+    $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedToolWindow
+    # Remove the close button by setting ControlBox to false
+    #$form.ControlBox = $false
+
     # Set regex patterns for IP address and CIDR
     $ipPattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     $cidrPattern = "^(3[0-2]|[1-2]?[0-9]|[0-9])$"
 
-    # Input box for form description
+    # Label for form description
     $formNameLabel = New-Object System.Windows.Forms.Label
     $formNameLabel.Location = New-Object System.Drawing.Point(10,10)
-    $formNameLabel.Size = New-Object System.Drawing.Size(410,40)
+    $formNameLabel.Size = New-Object System.Drawing.Size(460,20)
     $formNameLabel.Text = "Ensure all fields are filled in correctly and click the Submit button to proceed with the build."  
     
-    # Input box for info (model/cec/domain) tag at bottom of form
+    # Label for warning
+    $formWarningLabel = New-Object System.Windows.Forms.Label
+    $formWarningLabel.Location = New-Object System.Drawing.Point(10,30)
+    $formWarningLabel.Size = New-Object System.Drawing.Size(460,20)
+    $formWarningLabel.Font = New-Object System.Drawing.Font($formWarningLabel.Font, [System.Drawing.FontStyle]::Bold)
+    $formWarningLabel.Text = "WARNING: This will wipe the system drive and reinstall the Operating System!"  
+   
+
+    # Label for info (model/cec/domain) tag at bottom of form
     $formInfoLabel = New-Object System.Windows.Forms.Label
-    $formInfoLabel.Location = New-Object System.Drawing.Point(10,380)
+    $formInfoLabel.Location = New-Object System.Drawing.Point(10,400)
     $formInfoLabel.Size = New-Object System.Drawing.Size(490,40)
     $formInfoLabel.Text = "`nModel: $Model"
 
     # Radio buttons for server version selection
     $server2022RadioButton = New-Object System.Windows.Forms.RadioButton
     $server2022RadioButton.Location = New-Object System.Drawing.Point(10,60)
-    $server2022RadioButton.Size = New-Object System.Drawing.Size(100,20)
-    $server2022RadioButton.Text = "Server 2022"
+    $server2022RadioButton.Size = New-Object System.Drawing.Size(140,20)
+    $server2022RadioButton.Text = "Windows Server 2022"
     $server2025RadioButton = New-Object System.Windows.Forms.RadioButton
     $server2025RadioButton.Location = New-Object System.Drawing.Point(10,80)
-    $server2025RadioButton.Size = New-Object System.Drawing.Size(100,20)
-    $server2025RadioButton.Text = "Server 2025"
+    $server2025RadioButton.Size = New-Object System.Drawing.Size(140,20)
+    $server2025RadioButton.Text = "Windows Server 2025"
     $server2025RadioButton.Checked = $true
 
     # Input box for computer name
@@ -256,23 +269,29 @@
     #   Witness card form handling    #
     ###################################
 
-    # Input box for Witness computer name
+    # Input box for Witness description label
+    $WITLabel = New-Object System.Windows.Forms.Label
+    $WITLabel.Location = New-Object System.Drawing.Point(10,260)
+    $WITLabel.Size = New-Object System.Drawing.Size(460,30)
+    $WITLabel.Text = "XR4510c server detected, Witness sled configuration will be written to build USB as Witness.cfg in \Witness folder:"
+ 
+   # Input box for Witness Computername
     $WITcomputerNameLabel = New-Object System.Windows.Forms.Label
-    $WITcomputerNameLabel.Location = New-Object System.Drawing.Point(10,270)
-    $WITcomputerNameLabel.Size = New-Object System.Drawing.Size(330,20)
-    $WITcomputerNameLabel.Text = "XR4510c server detected, Witness sled configuration: `nWitness Server Name:"
+    $WITcomputerNameLabel.Location = New-Object System.Drawing.Point(10,300)
+    $WITcomputerNameLabel.Size = New-Object System.Drawing.Size(130,20)
+    $WITcomputerNameLabel.Text = "Witness Server Name:"
     $WITcomputerNameTextBox = New-Object System.Windows.Forms.TextBox
-    $WITcomputerNameTextBox.Location = New-Object System.Drawing.Point(150,290)
+    $WITcomputerNameTextBox.Location = New-Object System.Drawing.Point(150,300)
     $WITcomputerNameTextBox.Size = New-Object System.Drawing.Size(110,20)
     $WITcomputerNameTextBox.ReadOnly = $true
 
     # Input box for Witness IP Address
     $WITipAddressLabel = New-Object System.Windows.Forms.Label
-    $WITipAddressLabel.Location = New-Object System.Drawing.Point(10,310)
+    $WITipAddressLabel.Location = New-Object System.Drawing.Point(10,320)
     $WITipAddressLabel.Size = New-Object System.Drawing.Size(130,20)
     $WITipAddressLabel.Text = "Witness IP Address:"
     $WITipAddressTextBox = New-Object System.Windows.Forms.TextBox
-    $WITipAddressTextBox.Location = New-Object System.Drawing.Point(150,310)
+    $WITipAddressTextBox.Location = New-Object System.Drawing.Point(150,320)
     $WITipAddressTextBox.Size = New-Object System.Drawing.Size(110,20)
     $WITipAddressTextBox.BackColor = "Red"
     $WITipAddressTextBox.Add_TextChanged({
@@ -289,11 +308,11 @@
 
     # Input box for Witness Subnet CIDR
     $WITcidrLabel = New-Object System.Windows.Forms.Label
-    $WITcidrLabel.Location = New-Object System.Drawing.Point(10,330)
+    $WITcidrLabel.Location = New-Object System.Drawing.Point(10,340)
     $WITcidrLabel.Size = New-Object System.Drawing.Size(130,20)
     $WITcidrLabel.Text = "Witness Subnet CIDR:"
     $WITcidrTextBox = New-Object System.Windows.Forms.TextBox
-    $WITcidrTextBox.Location = New-Object System.Drawing.Point(150,330)
+    $WITcidrTextBox.Location = New-Object System.Drawing.Point(150,340)
     $WITcidrTextBox.Size = New-Object System.Drawing.Size(25,20)
     $WITcidrTextBox.BackColor = "Red"
     $WITcidrTextBox.Add_TextChanged({
@@ -315,11 +334,11 @@
     
     # Input box for Gateway IP (read-only)
     $WITgatewayLabel = New-Object System.Windows.Forms.Label
-    $WITgatewayLabel.Location = New-Object System.Drawing.Point(10,350)
+    $WITgatewayLabel.Location = New-Object System.Drawing.Point(10,360)
     $WITgatewayLabel.Size = New-Object System.Drawing.Size(130,20)
     $WITgatewayLabel.Text = "Witness Gateway IP:"
     $WITgatewayTextBox = New-Object System.Windows.Forms.TextBox
-    $WITgatewayTextBox.Location = New-Object System.Drawing.Point(150,350)
+    $WITgatewayTextBox.Location = New-Object System.Drawing.Point(150,360)
     $WITgatewayTextBox.Size = New-Object System.Drawing.Size(110,20)
     $WITgatewayTextBox.ReadOnly = $true
 
@@ -329,7 +348,7 @@
 
     # Button to Submit form
     $submitButton = New-Object System.Windows.Forms.Button
-    $submitButton.Location = New-Object System.Drawing.Point(300,370)
+    $submitButton.Location = New-Object System.Drawing.Point(370,440)
     $submitButton.Size = New-Object System.Drawing.Size(75,25)
     $submitButton.Text = "Submit"
     $submitButton.Add_Click({
@@ -341,16 +360,19 @@
             [System.Windows.Forms.MessageBox]::Show("Please fill in all the fields", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
             return
         } else {
-            $formValues = [PSCustomObject]@{
+            $global:formValues = [PSCustomObject]@{
                 BuildOSVer = if ($server2022RadioButton.Checked) { "Server 2022" } else { "Server 2025" }
-                ComputerName = $computerNameTextBox.Text
+                CompName = $computerNameTextBox.Text
                 IPAddress = $ipAddressTextBox.Text
                 CIDR = $cidrTextBox.Text
                 GatewayIP = $gatewayTextBox.Text
                 DNSIPs = $dnsTextBox.Text
+                CEC = $global:CEC
+                NetBiosDomain = $global:NetbiosDomain
+                OSDDomainName = $global:FQDN
                 CreateVMs = $createVMSCheckbox.Checked
             }
-            $formValues | ConvertTo-Json | Set-Content -Path $logPath\SOE_CaptureStartupVars.json -Force
+            $global:formValues | ConvertTo-Json | Set-Content -Path $logPath\SOE_CaptureStartupVars.json -Force
             $global:formSuccess = $true
             $form.Close()
         }
@@ -358,6 +380,7 @@
 
     # Add controls to form
     $form.Controls.Add($formNameLabel)
+    $form.Controls.Add($formWarningLabel)
     $form.Controls.Add($formInfoLabel)
     $form.Controls.Add($server2022RadioButton)
     $form.Controls.Add($server2025RadioButton)
@@ -375,8 +398,9 @@
     $form.Controls.Add($dnsTextBox)
     $form.Controls.Add($createVMSCheckbox)
     $form.Controls.Add($submitButton)
-    #If ($Model -eq 'PowerEdge XR4510c' -and $TSMedia -eq 'FullMedia' ) {
-    If ($Model -eq 'Virtual Machine' ) {
+    If ($Model -eq 'PowerEdge XR4510c' -and $TSMedia -eq 'FullMedia' ) {
+    #If ($Model -eq 'Virtual Machine' ) {
+        $form.Controls.Add($WITLabel)
         $form.Controls.Add($WITcomputerNameLabel)
         $form.Controls.Add($WITcomputerNameTextBox)
         $form.Controls.Add($WITipAddressLabel)
@@ -388,6 +412,19 @@
     }
     # Show the form
     $form.ShowDialog()
+    Write-Output $global:formValues | fl *
+
+    # Set values to SMS TS variables
+    $TSEnv.Value('BuildOSVer') = $formValues.BuildOSVer
+    $TSEnv.Value('OSDComputerName') = $formValues.CompName
+    $TSEnv.Value('IPAddress') = $formValues.IP    
+    $TSEnv.Value('CIDR') = $formValues.CIDR
+    $TSEnv.Value('GWIP') = $formValues.GatewayIP
+    $TSEnv.Value('DNSIPs') = $formValues.DNSIPs
+    $TSEnv.Value('CEC') = $formValues.CEC
+    $TSEnv.Value('NetBiosName') = $formValues.NetBiosDomain
+    $TSEnv.Value('OSDDomainName') = $formValues.DomainFQDN
+    $TSEnv.Value('CreateVMs') = $formValues.CreateVMs
 
     if ($global:formSuccess) {
         "Capture Startup Variables script completed succesfully, refer to CaptureStartupVars.json for vars captured via the form." | Out-File $logFile -Append
